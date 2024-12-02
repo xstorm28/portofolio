3084ecccbbd2ca0f40ffe45a43aae4e726c0bab1
xstorm28 <borrego2837@gmail.com>
2024-12-02 17:18:37 -0600

    pls

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,18 +1,12 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
-import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
   return (
     <html>
       <body>
-        <Error />
+        <Error statusCode={500} />  {/* Puedes personalizar el código de estado si es necesario */}
       </body>
     </html>
   );
